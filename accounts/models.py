9b4250f4eed22090d8c675a225327fff01ed62e0
vitor quintanilha barbosa <vitorqb@gmail.com>
2018-11-11 13:47:34 +0100

    Added account model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+import attr
+import django.db.models as m
+from mptt.models import MPTTModel, TreeForeignKey
+from common.models import NameField
 
-# Create your models here.
+
+@attr.s()
+class AccountFactory():
+    """ Encapsulates the creation of Account """
+    pass
+
+
+# !!!! TODO -> Add population of Account
+class Account(MPTTModel):
+    """ An Account, a tree structure to host transactions. """
+    name = NameField()
+    acc_type = m.ForeignKey('AccountType')
+    parent = TreeForeignKey(
+        'self',
+        on_delete=m.CASCADE,
+        null=True,
+        blank=True,
+        related_name="children"
+    )
+
+
+# !!!! TODO -> Add population of AccountType
+class AccountType(m.Model):
+    """ A possible account type """
+    name = NameField()
+    children_allowed = m.BooleanField()
+    movements_allowed = m.BooleanField()
+    new_accounts_allowed = m.BooleanField()

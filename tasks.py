295197471d1106ad5537b3298efe1408d5fbb012
vitor quintanilha barbosa <vitorqb@gmail.com>
2018-12-29 20:26:05 +0100

    Solved bug in tasks because of function signature

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
 import os
-from functools import wraps
+from decorator import decorate
 from invoke import task
 from invoke.tasks import Context
 from invoke.runners import Result
@@ -22,7 +22,7 @@ class PacsContext:
         self._context = context
 
     def run(self, cmd: str, pty: bool = False) -> Result:
-        return self._context.run(cmd)
+        return self._context.run(cmd, pty=pty)
 
     @contextmanager
     def cd(self, dir_: str):
@@ -30,18 +30,21 @@ class PacsContext:
 
     def run_manage(self, cmd: str, pty: bool = False):
         """ Runs a django management command """
-        self.run(f"python {MANAGE_PATH} {cmd}", pty)
+        self.run(f"python {MANAGE_PATH} {cmd}", pty=pty)
 
 
-def pacstask(f):
-    """ Same as invoke.task but parses a PacsContext instead of a
-    Context """
-    @wraps(f)
-    def wrapped(c, *args, **kwargs):
-        pacs_context = PacsContext(c)
-        return f(pacs_context, *args, **kwargs)
-
-    return task(wrapped)
+def pacstask(*task_args, **task_kwargs):
+    """
+    Same as @task, but parses the first argument to a PacsContext.
+    Differently thank tasks, must ALWAYS be called, even if with no options.
+    i.e. @pacstask() not @pacstask.
+    """
+    def decorator(f):
+        def _pacstask(f, *args, **kwargs):
+            return f(PacsContext(args[0]), *args[1:], **kwargs)
+        out = decorate(f, _pacstask)
+        return task(*task_args, **task_kwargs)(out)
+    return decorator
 
 
 #
@@ -59,38 +62,38 @@ def _populate_db(c):
 #
 # Tasks
 #
-@pacstask
+@pacstask()
 def unit_test(c, opts=""):
     """ Calls pytest for all unit tests. """
     _run_pytest(c, f"--ignore={FUNCTIONAL_TESTS_PATH} {opts}")
 
 
-@pacstask
+@pacstask()
 def func_test(c, opts=""):
     """ Calls functional tests for python """
     _run_pytest(c, f"{FUNCTIONAL_TESTS_PATH} {opts}")
 
 
-@pacstask
+@pacstask()
 def test(c, opts=""):
     """ Runs functional and unit tests """
     _run_pytest(c, f". {opts}")
 
 
-@pacstask
+@pacstask()
 def populate_db(c):
     """ Calls the management commands to populate the db """
     _populate_db(c)
 
 
-@pacstask
+@pacstask()
 def runserver(c):
     """ Runs the development server """
     _populate_db(c)
     c.run_manage("runserver_plus --print-sql", pty=True)
 
 
-@pacstask
+@pacstask()
 def migrate(c):
     """ Runs migrations """
     c.run_manage("migrate", pty=True)

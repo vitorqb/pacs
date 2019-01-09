8740f54950b89c53d6176a57994b7b2c4cd5c3af
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-01-09 18:56:05 +0100

    Added TODO

--- movements/views.py
+++ movements/views.py
@@ -8,6 +8,7 @@ from movements.filters import TransactionFilterSet
 
 class TransactionViewSet(ModelViewSet):
     # !!!! TODO -> also order by pk
+    # !!!! TODO -> Prefetch related to use less queries (currently 82!)
     queryset = Transaction.objects.order_by('-date').all()
     serializer_class = TransactionSerializer
     filter_backends = (filters.DjangoFilterBackend,)

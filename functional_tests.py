a3aab113424f2421322921454dd9d8bf04b3e7ea
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-01-10 22:03:29 +0100

    Improved functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -19,38 +19,6 @@ from movements.serializers import TransactionSerializer
 from movements.tests.factories import TransactionTestFactory
 
 
-@attr.s()
-class _TestRequests():
-    """ A wrapper around `requests` to make requests for the
-    testing server """
-
-    # The base url
-    url = attr.ib()
-
-    # Default headers sent in every request
-    headers = attr.ib()
-
-    def get(self, path):
-        return requests.get(f"{self.url}{path}", headers=self.headers)
-
-    def post(self, path, json={}):
-        return requests.post(
-            f"{self.url}{path}",
-            json=json,
-            headers=self.headers
-        )
-
-    def patch(self, path, json={}):
-        return requests.patch(
-            f"{self.url}{path}",
-            json=json,
-            headers=self.headers
-        )
-
-    def delete(self, path):
-        return requests.delete(f"{self.url}{path}", headers=self.headers)
-
-
 class FunctionalTests(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -91,45 +59,32 @@ class FunctionalTests(StaticLiveServerTestCase):
         self.assert_response_status_okay(resp)
         return resp.json()
 
-    def assert_is_acc_name(self, name):
-        """ Asserts that name is an acc name returned by a get request """
-        accs = self.get_json('/accounts/')
-        assert any(x['name'] == name for x in accs), \
-            f"'{name}' not found on account names '{accs}'"
-
-    def assert_is_not_acc_name(self, name):
-        """ Asserts that name is not an acc name returned by a get request """
-        accs = self.get_json('/accounts/')
-        assert all(x['name'] != name for x in accs), \
-            f"'{name}' found on account names '{accs}'"
-
     def test_unlogged_user_cant_make_queries(self):
         # The user tries to make a query without the header and sees 403
-        url = f'/accounts/'
         self.requests.headers = {}
-        assert self.requests.get(url).status_code == 403,\
+        assert self.requests.get(URLS.account).status_code == 403,\
             "User should have been unauthorized because of no header!"
 
         # Then he puts the correct token and it works!
         self.requests.headers = {'Authorization': f'Token {settings.ADMIN_TOKEN}'}
-        assert self.requests.get(url).status_code == 200, \
+        assert self.requests.get(URLS.account).status_code == 200, \
             "User should have been successfull becase he has the header"
 
     def test_creates_an_account_hierarchy(self):
         # The user enters and only sees the default accounts there
-        resp_accs = self.get_json('/accounts/')
+        resp_accs = self.get_json(URLS.account)
         assert len(resp_accs) == len(ACCOUNT_DATA)
         assert set(x['name'] for x in resp_accs) == \
             set(x['name'] for x in ACCOUNT_DATA)
 
         # The user decides to add Expenses and Supermarket
-        root_acc_pk = next(x['pk'] for x in resp_accs if x['acc_type'] == 'Root')
+        root_acc = _find_root(resp_accs)
         expenses_acc = {
             "name": "Expenses",
             "acc_type": "Branch",
-            "parent": root_acc_pk
+            "parent": root_acc['pk']
         }
-        expenses_json = self.post_json("/accounts/", expenses_acc)
+        expenses_json = self.post_json(URLS.account, expenses_acc)
 
         expenses_pk = expenses_json['pk']
         supermarket_acc = {
@@ -137,11 +92,16 @@ class FunctionalTests(StaticLiveServerTestCase):
             "acc_type": "Leaf",
             "parent": expenses_pk
         }
-        resp = self.post_json("/accounts/", supermarket_acc)
+        resp = self.post_json(URLS.account, supermarket_acc)
 
         assert resp['parent'] == expenses_pk
         assert resp['acc_type'] == "Leaf"
         assert resp['name'] == "Supermarket"
+
+        # And he can see them
+        accounts = self.get_json(URLS.account)
+        for x in 'Expenses', 'Supermarket':
+            _assert_contains(accounts, 'name', x)
 
     def test_user_changes_name_of_account(self):
         # The user had previously creates an account
@@ -149,17 +109,17 @@ class FunctionalTests(StaticLiveServerTestCase):
         AccountTestFactory(name=orig_name)
 
         # Which he sees when he opens the app
-        accs_json = self.get_json("/accounts/")
-        assert orig_name in set(x['name'] for x in accs_json)
-        acc_data = next(x for x in accs_json if x['name'] == orig_name)
+        accounts = self.get_json(URLS.account)
+        acc_data = next(x for x in accounts if x['name'] == orig_name)
 
         # It now decides to change the name
         new_name = "Current Account (La Caixa)"
-        self.patch_json(f"/accounts/{acc_data['pk']}/", {"name": new_name})
+        self.patch_json(f"{URLS.account}{acc_data['pk']}/", {"name": new_name})
 
         # And he sees it worked, and he is happy
-        self.assert_is_acc_name(new_name)
-        self.assert_is_not_acc_name(orig_name)
+        accounts = self.get_json(URLS.account)
+        _assert_contains(accounts, 'name', new_name)
+        _assert_not_contains(accounts, 'name', orig_name)
 
     def test_user_changes_account_hierarchy(self):
         # The user had previously created an Current Account whose
@@ -181,41 +141,44 @@ class FunctionalTests(StaticLiveServerTestCase):
 
         # It currects the name of the existant account
         self.patch_json(
-            f"/accounts/{cur_acc.pk}/",
+            f"{URLS.account}{cur_acc.pk}/",
             {"name": "Current Account Itau"}
         )
         # And sees that it worked
-        self.assert_is_acc_name("Current Account Itau")
-        self.assert_is_not_acc_name("Current Account")
+        accounts = self.get_json(URLS.account)
+        _assert_contains(accounts, 'name', 'Current Account Itau')
+        _assert_not_contains(accounts, 'name', "Current Account")
 
         # He creates the new father for it
-        new_cur_acc_data = {
+        new_father_data = {
             "name": "Current Account",
             "parent": root.pk,
             "acc_type": "Branch"
         }
-        resp_data = self.post_json("/accounts/", new_cur_acc_data)
-        new_cur_acc_pk = resp_data['pk']
+        new_father = self.post_json(URLS.account, new_father_data)
         # And sees that it worked
-        self.assert_is_acc_name(new_cur_acc_data['name'])
+        accounts = self.get_json(URLS.account)
+        _assert_contains(accounts, 'name', new_father_data['name'])
 
         # He sets the old acc to have this father
         resp_data = self.patch_json(
-            f"/accounts/{cur_acc.pk}/",
-            json={"parent": new_cur_acc_pk}
+            f"{URLS.account}{cur_acc.pk}/",
+            json={"parent": new_father['pk']}
         )
-        assert resp_data['parent'] == new_cur_acc_pk
+        assert resp_data['parent'] == new_father['pk']
 
         # And creates the new account
         self.post_json(
-            "/accounts/",
+            URLS.account,
             json={
                 "name": "Current Account LaCaixa",
-                "parent": new_cur_acc_pk,
+                "parent": new_father['pk'],
                 "acc_type": "Leaf"
             }
         )
-        self.assert_is_acc_name("Current Account LaCaixa")
+
+        accounts = self.get_json(URLS.account)
+        _assert_contains(accounts, 'name', "Current Account LaCaixa")
 
     def test_first_transaction(self):
         # The user creates two accounts
@@ -232,12 +195,12 @@ class FunctionalTests(StaticLiveServerTestCase):
             }
         ]
         accs = [
-            self.post_json("/accounts/", raw_data) for raw_data in accs_raw_data
+            self.post_json(URLS.account, raw_data) for raw_data in accs_raw_data
         ]
 
         # And the Yen currency
         euro_raw_data = {"name": "Yen"}
-        euro = self.post_json("/currencies/", euro_raw_data)
+        euro = self.post_json(URLS.currency, euro_raw_data)
 
         # And it's first transaction ever!
         trans_raw_data = {
@@ -260,10 +223,10 @@ class FunctionalTests(StaticLiveServerTestCase):
                 },
             ]
         }
-        self.post_json("/transactions/", trans_raw_data)
+        self.post_json(URLS.transaction, trans_raw_data)
 
         # Which now appears when querying for all transactions
-        get_trans_resp = self.get_json("/transactions/")
+        get_trans_resp = self.get_json(URLS.transaction)
         assert len(get_trans_resp) == 1
         assert get_trans_resp[0]['date'] == trans_raw_data['date']
 
@@ -303,12 +266,12 @@ class FunctionalTests(StaticLiveServerTestCase):
 
         # He queries ony for transactions involving acc1, and see the
         # same ones listed, in chronological order
-        assert self.get_json(f"/transactions/?account_id={accs[0].pk}") == \
+        assert self.get_json(f"{URLS.transaction}?account_id={accs[0].pk}") == \
             serialized_transactions
 
         # He adds a new transaction of 10 cur to acc2
         new_transaction = self.post_json(
-            f"/transactions/",
+            URLS.transaction,
             {
                 "description": "New Transaction",
                 "date": "2018-01-03",
@@ -334,32 +297,27 @@ class FunctionalTests(StaticLiveServerTestCase):
 
         # He queries again for transactions involving acc1, and see all
         # of them listed
-        assert self.get_json(f"/transactions/?account_id={accs[0].pk}") == \
+        assert self.get_json(f"{URLS.transaction}?account_id={accs[0].pk}") == \
             serialized_transactions
 
     def test_get_account_journal(self):
         # The user creates two accounts
-        # !!!! TODO -> Method to query for root account
-        root_acc = next(
-            x for x in self.get_json("/accounts/") if x['acc_type'] == 'Root'
-        )
+        root_acc = _find_root(self.get_json(URLS.account))
         cash_account_data = {
             "name": "Cash",
             "acc_type": "Leaf",
             "parent": root_acc['pk']
         }
-        cash_account = self.post_json('/accounts/', cash_account_data)
+        cash_account = self.post_json(URLS.account, cash_account_data)
         bank_account_data = {
             "name": "Bank",
             "acc_type": "Leaf",
             "parent": root_acc['pk']
         }
-        bank_account = self.post_json('/accounts/', bank_account_data)
+        bank_account = self.post_json(URLS.account, bank_account_data)
 
         # And two transactions
-        euro = next(
-            x for x in self.get_json("/currencies/") if x['name'] == "Euro"
-        )
+        euro = _select_by(self.get_json(URLS.currency), 'name', 'Euro')
         withdrawal_data = {
             "description": "withdrawal",
             "date": "2018-01-01",
@@ -380,7 +338,7 @@ class FunctionalTests(StaticLiveServerTestCase):
                 }
             ]
         }
-        withdrawal = self.post_json('/transactions/', withdrawal_data)
+        withdrawal = self.post_json(URLS.transaction, withdrawal_data)
         deposit_data = {
             "description": "deposit",
             "date": "2018-01-02",
@@ -401,10 +359,10 @@ class FunctionalTests(StaticLiveServerTestCase):
                 }
             ]
         }
-        deposit = self.post_json('/transactions/', deposit_data)
+        deposit = self.post_json(URLS.transaction, deposit_data)
 
         # It queries for the journal of the bank account
-        journal = self.get_json(f'/accounts/{bank_account["pk"]}/journal/')
+        journal = self.get_json(f'{URLS.account}{bank_account["pk"]}/journal/')
 
         # It sees the account pk and both transactions there
         assert journal['account'] == bank_account['pk']
@@ -421,3 +379,67 @@ class FunctionalTests(StaticLiveServerTestCase):
         # And the transactions
         assert journal['transactions'][0] == withdrawal
         assert journal['transactions'][1] == deposit
+
+
+#
+# Helpers
+#
+class URLS:
+    account = '/accounts/'
+    currency = '/currencies/'
+    transaction = '/transactions/'
+
+
+def _find_root(acc_list):
+    """ Returns the root account out of a list of accounts """
+    return next(a for a in acc_list if a['acc_type'] == 'Root')
+
+
+def _assert_contains(list_, key, value):
+    """ Asserts that one of the dictionaries in list_ has a key whose
+    value is equal to name """
+    assert any(x[key] == value for x in list_), \
+        f"{value} not found for key {key} in list {list_}"
+
+
+def _assert_not_contains(list_, key, value):
+    """ Opposite of _assert_contains """
+    assert not any(x[key] == value for x in list_), \
+        f"{value} WAS FOUND for key {key} in list {list_}"
+
+
+def _select_by(list_, key, value):
+    """ Selects the (first) dict from list_ that has value in it's key """
+    return next(x for x in list_ if x[key] == value)
+
+
+@attr.s()
+class _TestRequests():
+    """ A wrapper around `requests` to make requests for the
+    testing server """
+
+    # The base url
+    url = attr.ib()
+
+    # Default headers sent in every request
+    headers = attr.ib()
+
+    def get(self, path):
+        return requests.get(f"{self.url}{path}", headers=self.headers)
+
+    def post(self, path, json={}):
+        return requests.post(
+            f"{self.url}{path}",
+            json=json,
+            headers=self.headers
+        )
+
+    def patch(self, path, json={}):
+        return requests.patch(
+            f"{self.url}{path}",
+            json=json,
+            headers=self.headers
+        )
+
+    def delete(self, path):
+        return requests.delete(f"{self.url}{path}", headers=self.headers)

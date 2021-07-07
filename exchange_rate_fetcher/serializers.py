2a140a61441e976ffe91afb0dba4ab4824b85ed4
vitor quintanilha barbosa <vitorqb@gmail.com>
2021-07-07 15:19:01 +0200

    OPT Moves CurrencyCodesField to common

--- exchange_rate_fetcher/serializers.py
+++ exchange_rate_fetcher/serializers.py
@@ -1,21 +1,13 @@
 from rest_framework import serializers
 from .view_models import ExchangeRateDataInputs
 import common.models
-
-
-class CurrencyCodesField(serializers.Field):
-    """ Converts a string with comas into a list. """
-
-    def to_internal_value(self, data):
-        if not isinstance(data, str):
-            raise serializers.ValidationError("Should be a string!")
-        return data.split(",")
+import common.serializers
 
 
 class ExchangeRateDataInputsSerializer(serializers.Serializer):
     start_at = common.models.new_string_date_field()
     end_at = common.models.new_string_date_field()
-    currency_codes = CurrencyCodesField()
+    currency_codes = common.serializers.CurrencyCodesField()
     token = serializers.CharField(required=False, allow_null=True)
 
     def create(self, data):
--- exchange_rate_fetcher/tests/test_serializers.py
+++ exchange_rate_fetcher/tests/test_serializers.py
@@ -2,18 +2,3 @@ from unittest import TestCase
 import exchange_rate_fetcher.serializers as sut
 import pytest
 from rest_framework.validators import ValidationError
-
-
-class CurrencyCodesFieldTest(TestCase):
-
-    def test_valid(self):
-        valid_value = "A,BBB,CCCCC"
-        field = sut.CurrencyCodesField()
-        result = field.run_validation(valid_value)
-        assert result == ["A", "BBB", "CCCCC"]
-
-    def test_invalid(self):
-        invalid_value = 1
-        field = sut.CurrencyCodesField()
-        with pytest.raises(ValidationError):
-            field.run_validation(invalid_value)

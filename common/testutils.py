3af4111516a4abd1e4b23e65cc7457aef5d53917
vitor quintanilha barbosa <vitorqb@gmail.com>
2021-12-22 13:40:58 +0100

    OPT Moves DataMaker to testutils

--- common/testutils.py
+++ common/testutils.py
@@ -135,3 +135,179 @@ def populate_exchangerates_with_mock_data():
             {"currency_code": "BRL", "date": "2020-01-06", "value": Decimal('0.2')}
     ):
         common.models.full_clean_and_save(exchangerates.models.ExchangeRate(**x))
+
+
+@attr.s()
+class DataMaker:
+    """ Helper to make json data for the requests """
+
+    # The root account
+    root_acc = attr.ib()
+
+    def expenses_acc(self):
+        return {
+            "name": "Expenses",
+            "acc_type": "Branch",
+            "parent": self.root_acc['pk']
+        }
+
+    def revenues_acc(self):
+        return {
+            'name': 'Revenue',
+            'acc_type': 'Branch',
+            'parent': self.root_acc['pk'],
+        }
+
+    def supermarket_acc(self, parent):
+        return {
+            "name": "Supermarket",
+            "acc_type": "Leaf",
+            "parent": parent['pk']
+        }
+
+    def current_acc(self, parent):
+        return {
+            "name": "Current Account",
+            "acc_type": "Leaf",
+            "parent": parent["pk"]
+        }
+
+    def assets_acc(self):
+        return {
+            "name": "Assets",
+            "acc_type": "Branch",
+            "parent": self.root_acc["pk"]
+        }
+
+    def salary_acc(self, parent):
+        return {
+            "name": "Salary",
+            "acc_type": "Leaf",
+            "parent": parent["pk"]
+        }
+
+    def money_acc(self, parent):
+        return {
+            "name": "Money",
+            "acc_type": "Leaf",
+            "parent": parent['pk']
+        }
+
+    def salary_tra(self, date, from_acc, to_acc, quantity, curr):
+        return {
+            "description": "Salary",
+            "date": date,
+            "movements_specs": [
+                {
+                    "account": from_acc['pk'],
+                    "money": {
+                        "quantity": -quantity,
+                        "currency": curr['pk'],
+                    },
+                },
+                {
+                    "account": to_acc['pk'],
+                    "money": {
+                        "quantity": quantity,
+                        "currency": curr['pk'],
+                    },
+                },
+            ]
+        }
+
+    def earn_money_tra(self, from_acc, to_acc, curr):
+        return {
+            "description": "Earned some money!",
+            "date": "2018-01-01",
+            "tags": [
+                {
+                    "name": "earning-type",
+                    "value": "ocasional",
+                }
+            ],
+            "movements_specs": [
+                {
+                    "account": from_acc['pk'],
+                    "money": {
+                        "quantity": -1000,
+                        "currency": curr['pk']
+                    }
+                },
+                {
+                    "account": to_acc['pk'],
+                    "money": {
+                        "quantity": 1000,
+                        "currency": curr['pk']
+                    }
+                },
+            ]
+        }
+
+    def deposit(self, accbank, accmoney, curr, date_=None):
+        return {
+            "description": "Deposit",
+            "date": date_ or "2018-02-11",
+            "movements_specs": [
+                {
+                    "account": accmoney['pk'],
+                    "money": {
+                        "quantity": -1000,
+                        "currency": curr['pk']
+                    }
+                },
+                {
+                    "account": accbank['pk'],
+                    "money": {
+                        "quantity": 1000,
+                        "currency": curr['pk']
+                    }
+                },
+            ]
+        }
+
+    def withdrawal(self, accbank, accmoney, curr, date_=None):
+        return {
+            "description": "Withdrawal",
+            "date": date_ or "2018-03-11",
+            "movements_specs": [
+                {
+                    "account": accbank['pk'],
+                    "money": {
+                        "quantity": "-120",
+                        "currency": curr['pk']
+                    }
+                },
+                {
+                    "account": accmoney['pk'],
+                    "money": {
+                        "quantity": "120",
+                        "currency": curr['pk']
+                    }
+                },
+            ]
+        }
+
+    def paid_supermarket(self, accfrom, accto, curr, date_=None, reference=None):
+        out = {
+            "description": "Supermarket!",
+            "date": date_ or "2017-12-21",
+            "movements_specs": [
+                {
+                    "account": accfrom['pk'],
+                    "money": {
+                        "quantity": "-120",
+                        "currency": curr['pk']
+                    }
+                },
+                {
+                    "account": accto['pk'],
+                    "money": {
+                        "quantity": "120",
+                        "currency": curr['pk']
+                    }
+                },
+            ]
+        }
+        if reference:
+            out['reference'] = reference
+        return out
